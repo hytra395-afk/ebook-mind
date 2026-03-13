3c567b2fd0162fa3aea78c874cafee0c68ffc321
antevasinnguyen <antevasinnguyen@gmail.com>
2026-03-13 09:33:11 +0700

    Add licenses table to foreign key cascade migration

--- migrations/004_fix_foreign_keys_cascade.sql
+++ migrations/004_fix_foreign_keys_cascade.sql
@@ -8,6 +8,7 @@ ALTER TABLE reviews DROP CONSTRAINT IF EXISTS reviews_ebook_id_fkey;
 ALTER TABLE reviews DROP CONSTRAINT IF EXISTS reviews_combo_id_fkey;
 ALTER TABLE combo_items DROP CONSTRAINT IF EXISTS combo_items_ebook_id_fkey;
 ALTER TABLE combo_items DROP CONSTRAINT IF EXISTS combo_items_combo_id_fkey;
+ALTER TABLE licenses DROP CONSTRAINT IF EXISTS licenses_ebook_id_fkey;
 
 -- Add new foreign key constraints with ON DELETE CASCADE
 ALTER TABLE order_items
@@ -33,3 +34,7 @@ ALTER TABLE combo_items
 ALTER TABLE combo_items
   ADD CONSTRAINT combo_items_combo_id_fkey 
   FOREIGN KEY (combo_id) REFERENCES combos(id) ON DELETE CASCADE;
+
+ALTER TABLE licenses
+  ADD CONSTRAINT licenses_ebook_id_fkey 
+  FOREIGN KEY (ebook_id) REFERENCES ebooks(id) ON DELETE CASCADE;
