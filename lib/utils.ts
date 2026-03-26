0e6b5a69cac6e599f78f18b0a744181baacc1c65
antevasinnguyen <antevasinnguyen@gmail.com>
2026-03-26 09:16:09 +0700

    fix: use Google Drive thumbnail API for better image compatibility

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 /**
  * Convert Google Drive share URL to direct image URL
  * Input:  https://drive.google.com/file/d/FILE_ID/view?usp=sharing
- * Output: https://lh3.googleusercontent.com/d/FILE_ID (CORS-friendly)
+ * Output: https://drive.google.com/thumbnail?id=FILE_ID&sz=w1000 (Works with public files)
  */
 export function convertDriveUrl(url: string): string {
   if (!url) return url
@@ -16,13 +16,13 @@ export function convertDriveUrl(url: string): string {
   // Handle: https://drive.google.com/file/d/FILE_ID/view?...
   const fileMatch = url.match(/drive\.google\.com\/file\/d\/([a-zA-Z0-9_-]+)/)
   if (fileMatch) {
-    return `https://lh3.googleusercontent.com/d/${fileMatch[1]}`
+    return `https://drive.google.com/thumbnail?id=${fileMatch[1]}&sz=w1000`
   }
 
   // Handle: https://drive.google.com/open?id=FILE_ID
   const openMatch = url.match(/drive\.google\.com\/open\?id=([a-zA-Z0-9_-]+)/)
   if (openMatch) {
-    return `https://lh3.googleusercontent.com/d/${openMatch[1]}`
+    return `https://drive.google.com/thumbnail?id=${openMatch[1]}&sz=w1000`
   }
 
   // If already converted or other format, return as is
