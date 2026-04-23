b74d94aa7ffdeb9ea00534f915efd3bf5c2ca723
antevasinnguyen <antevasinnguyen@gmail.com>
2026-04-23 20:00:26 +0700

    Add script-src-elem to CSP policy to allow Google Analytics scripts to load

--- middleware.ts
+++ middleware.ts
@@ -74,6 +74,7 @@ export function middleware(request: NextRequest) {
     [
       "default-src 'self'",
       "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://connect.facebook.net",
+      "script-src-elem 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' data: https: http:",
